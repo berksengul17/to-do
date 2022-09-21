cacb3d69f676096d73a9cbe87e73df18df469d71
Berk Şengül <berksengul17@gmail.com>
2022-09-21 16:13:50 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To-do",
+      templat: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.css$/",
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: "/.(png|svg|jpg|jpeg|gif)$/",
+        type: "asset/resource",
+      },
+      {
+        test: "/.(woff|woff2|ttf|eot|otf)$/",
+        type: "asset/resource",
+      },
+    ],
+  },
+};
