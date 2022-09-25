b8877b934af8a0fc29ccaed9f86dd7ccadd77aa4
Berk Şengül <berksengul17@gmail.com>
2022-09-25 09:45:09 +0300

    Create event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import "./styles/common.css";
+import reset from "./functions/reset.js";
 import sidebar from "./pages/sidebar.js";
+import header from "./pages/header.js";
+import inbox from "./pages/inbox.js";
+import today from "./pages/today.js";
+import upcoming from "./pages/upcoming.js";
 
+// Initial call
+header();
 sidebar();
+inbox();
+
+const inboxLink = document.querySelector(".inbox-link");
+const todayLink = document.querySelector(".today-link");
+const upcomingLink = document.querySelector(".upcoming-link");
+
+inboxLink.addEventListener("click", () => {
+  reset();
+  inbox();
+  inboxLink.classList.add("active");
+});
+
+todayLink.addEventListener("click", () => {
+  reset();
+  today();
+  todayLink.classList.add("active");
+});
+
+upcomingLinkaddEventListener("click", () => {
+  reset();
+  upcoming();
+  upcomingLink.classList.add("active");
+});
